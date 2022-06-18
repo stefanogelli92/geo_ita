c9a6ca57c3a8b1aa879491cbe692ebb9a53b4270
A470222 <G4b41e2!>
2022-06-18 12:14:15 +0200

    Add files to git

---
+++ build/lib/geo_ita/data.py
@@ -0,0 +1,3 @@
+from geo_ita.src._data import (get_list_comuni, get_list_province, get_list_regioni,
+                               get_df_comuni, get_df_province, get_df_regioni,
+                               get_high_resolution_population_density_df)
