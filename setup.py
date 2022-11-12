4c49252369a87cea50f5d124b23c2808fbe0f81f
stefanogelli92 <1Egnegne1>
2022-11-12 23:12:23 +0100

    add whl

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["pandas", "twine", "geopandas", "geopy",
+requirements = ["pandas", "twine", "geopandas>=0.12.1", "geopy",
                 "matplotlib>=3.1.1", "numpy", "descartes",
                 "xlrd", "openpyxl", "unidecode", "pyproj", "rtree",
-                "bokeh", "requests", "beautifulsoup4", "google", "scipy",
+                "bokeh>=3.0.1", "requests", "beautifulsoup4", "google", "scipy",
                 "google-api-python-client", "valdec", "pydantic"]
 
 setup(
     name="geo_ita",
-    version="0.0.2",
+    version="0.0.1",
     author="Stefano Gelli",
     author_email="stefano.mat92@gmail.com",
     description="A package for geo analysis for Italy",
