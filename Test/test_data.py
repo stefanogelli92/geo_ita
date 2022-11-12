7a02e21421dcc853534eac9e07f6ac07706606ba
stefanogelli92 <1Egnegne1>
2022-11-12 12:12:42 +0100

    add test

--- Test/test_data.py
+++ Test/test_data.py
@@ -11,44 +11,46 @@ from pathlib import PureWindowsPath
 
 class TestEnrichDataframe(unittest.TestCase):
 
-    def xtest_get_df_comuni(self):
+    def test_get_df_comuni(self):
         df = get_df_comuni()
         self.assertTrue(isinstance(df, pd.DataFrame))
         self.assertGreater(df.shape[0], 0)
 
-    def xtest_get_df_province(self):
+    def test_get_df_province(self):
         df = get_df_province()
         self.assertTrue(isinstance(df, pd.DataFrame))
         self.assertGreater(df.shape[0], 0)
 
-    def xtest_get_df_regioni(self):
+    def test_get_df_regioni(self):
         df = get_df_regioni()
         self.assertTrue(isinstance(df, pd.DataFrame))
         self.assertGreater(df.shape[0], 0)
 
-    def xtest_get_list_comuni(self):
+    def test_get_list_comuni(self):
         result = get_list_comuni()
         self.assertTrue(isinstance(result, list))
         self.assertGreater(len(result), 0)
 
-    def xtest_get_list_province(self):
+    def test_get_list_province(self):
         result = get_list_province()
         self.assertTrue(isinstance(result, list))
         self.assertGreater(len(result), 0)
 
-    def xtest_get_list_regioni(self):
+    def test_get_list_regioni(self):
         result = get_list_regioni()
         self.assertTrue(isinstance(result, list))
         self.assertGreater(len(result), 0)
 
-    def xtest_download_high_density_population_df(self):
+    def test_download_high_density_population_df(self):
         remove_high_resolution_population_density_file()
         df = get_high_resolution_population_density_df()
         del df
         df = get_high_resolution_population_density_df()
+        self.assertTrue(isinstance(df, pd.DataFrame))
+        self.assertGreater(df.shape[0], 0)
 
     def test_upload_data_istat_from_api(self):
-        df = upload_data_istat_from_api()
+        upload_data_istat_from_api()
 
 
 if __name__ == '__main__':
